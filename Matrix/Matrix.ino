e73d1756b78a32711e06758ad26789cb5a40b384
Joran <jorancuypers@gmail.com>
2019-03-08 10:06:14 +0100

    Update Matrix.ino

--- Matrix/Matrix.ino
+++ Matrix/Matrix.ino
@@ -1,11 +1,18 @@
 #include "ShiftRegister74HC595.h"
+// create a global shift register object
+// parameters: (number of shift registers, data pin, clock pin, latch pin)
+ShiftRegister74HC595 sr (1, 0, 1, 2);
+
 
 void setup() {
   // put your setup code here, to run once:
+  
+  Serial.begin(115200);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  sr.set(1, HIGH);
 
 }
