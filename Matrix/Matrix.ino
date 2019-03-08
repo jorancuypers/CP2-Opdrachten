1d4590cab654a33b6e0c6768bb8d278d1abc52de
Joran <jorancuypers@gmail.com>
2019-03-08 12:22:34 +0100

    commit

--- Matrix/Matrix.ino
+++ Matrix/Matrix.ino
@@ -1,18 +1,54 @@
-#include "ShiftRegister74HC595.h"
-// create a global shift register object
-// parameters: (number of shift registers, data pin, clock pin, latch pin)
-ShiftRegister74HC595 sr (1, 0, 1, 2);
+/**
+* Auteur: Stijn Boutsen
+*
+* Beschrijving: Dit programma laat de led matrix knipperen op het PXL uCbord.
+*
+*/
 
+int IMAGE[8][8]={{0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0,}};
+
+
+//Definiëren van de uitganspinnen
+const int shiftClockPin = 3;  //SH
+const int latchClockPin = 4;  //ST
+const int serialInputPin = 9; //DS
+
+//K5 K3 K6 R4 K8 R6 R7 K7
+byte bericht1 = 0b11100000;
+//R1 R2 K2 R8 K4 R3 R5 K1
+byte bericht2 = 0b11011000;
+boolean shiftLeftRight = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  
+  //Instelen van de uitgangspinnen
+  pinMode(shiftClockPin, OUTPUT);
+  pinMode(latchClockPin, OUTPUT);
+  pinMode(serialInputPin, OUTPUT);
   Serial.begin(115200);
-
 }
 
 void loop() {
+  displayData(bericht1);
+  displayData(bericht2);
+  
+  delay(200);
+}
+
+void displayData(byte message) {
   // put your main code here, to run repeatedly:
-  sr.set(1, HIGH);
+  for (int i = 0; i <= 7; i++) {
+    digitalWrite(serialInputPin, bitRead(message, i));
+    digitalWrite(shiftClockPin, HIGH);
+    digitalWrite(shiftClockPin, LOW);
+  }
+  digitalWrite(latchClockPin, HIGH);
+  digitalWrite(latchClockPin, LOW);
 
 }
