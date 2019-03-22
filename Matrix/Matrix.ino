8752628280930ee37250996bec8efeb96cd42a5b
Joran Cuypers <47535831+jorancuypers@users.noreply.github.com>
2019-03-22 12:35:24 +0100

    Update Matrix.ino

--- Matrix/Matrix.ino
+++ Matrix/Matrix.ino
@@ -1,11 +1,13 @@
-int IMAGE[8][8]={{0,0,0,0,0,0,0,0},
+int IMAGE[8][8]={{1,0,0,0,0,0,0,0},
+                 {1,0,0,0,0,0,0,0},
+                 {0,0,0,0,0,0,0,0},
+                 {1,0,0,0,0,0,0,0},
                  {0,0,0,0,0,0,0,0},
                  {0,0,0,0,0,0,0,0},
                  {0,0,0,0,0,0,0,0},
-                 {0,0,0,0,0,0,0,0},
-                 {0,0,0,0,0,0,0,0},
-                 {0,0,0,0,0,0,0,0},
-                 {0,0,0,0,0,0,0,0,}};
+                 {0,0,0,0,0,0,0,0}};
+
+
 
 //Definiëren van de uitganspinnen
 const int shiftClockPin = 3;  //SH
@@ -29,7 +31,7 @@ const int serialInputPin = 9; //DS
 //R7 - 15
 //K7 - 16
 
-word bericht1  = 0b1001000001000000;
+word bericht1  = 0b1101011000010110;
 word bericht2  = 0b1101110000000110;
 word bericht3  = 0b1101001010010010;
 word bericht4  = 0b1001011000110100;
@@ -46,14 +48,8 @@ void setup() {
 
 void loop()
 {
-
+ Translate();
  displayData(bericht1);
- delay(1);
- displayData(bericht2);
- delay(1);
- displayData(bericht3);
- delay(1);
- displayData(bericht4);
  delay(1);
 
 }
@@ -70,3 +66,40 @@ void displayData(word message)
   digitalWrite(latchClockPin, HIGH);
   digitalWrite(latchClockPin, LOW);
 }
+
+
+
+void Translate(){
+  int y = 0;
+  if (IMAGE[0][0] == 1){
+    bericht1 = setBits(8, 1, bericht1);
+    bericht1 = setBits(1, 0, bericht1);
+    }
+  if (IMAGE[1][0] == 1){
+    bericht1 = setBits(8, 1, bericht1);
+    bericht1 = setBits(2, 0, bericht1);
+    }
+  if (IMAGE[2][0] == 1){
+    bericht1 = setBits(8, 1, bericht1);
+    bericht1 = setBits(6, 0, bericht1);
+    }
+  if (IMAGE[3][0] == 1){
+    bericht1 = setBits(8, 1, bericht1);
+    bericht1 = setBits(12, 0, bericht1);
+    }
+  }
+
+
+
+
+  
+short int setBits(int welkeBit,int waardeBit,word X){
+  if (waardeBit ==1){
+  return(X | (1<<(16-welkeBit)));
+  }
+  else{
+    return (X ^ (1<<(16-welkeBit)));
+    }
+}
+
+//Thanks aan eerlijke toetelscams
