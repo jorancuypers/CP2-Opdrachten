f2a4346ddf2d66b10e0a227acdac80f4ce49c99f
Joran Cuypers <47535831+jorancuypers@users.noreply.github.com>
2019-03-26 14:21:48 +0100

    Update Matrix.ino

--- Matrix/Matrix.ino
+++ Matrix/Matrix.ino
@@ -7,14 +7,6 @@ byte IMAGE[8][8]={{0,0,0,0,0,0,0,0},
                  {0,0,0,0,0,0,0,0},
                  {0,0,0,0,0,0,0,0}};
 
-byte J[8][8]={{0,0,0,0,0,0,0,0},
-             {0,1,1,1,1,1,1,0},
-             {0,0,0,0,0,0,1,0},
-             {0,0,0,0,0,0,1,0},
-             {0,0,0,0,0,0,1,0},
-             {0,0,0,0,0,0,1,0},
-             {0,0,1,0,0,1,1,0},
-             {0,0,0,1,1,0,0,0}};
 
 byte A[8][8]=    {{0,0,0,0,0,0,0,0},
                  {0,0,0,1,1,0,0,0},
@@ -25,6 +17,99 @@ byte A[8][8]=    {{0,0,0,0,0,0,0,0},
                  {0,1,0,0,0,0,1,0},
                  {0,1,0,0,0,0,1,0}};
 
+byte B[8][8]={{0,0,0,0,0,0,0,0},
+              {0,1,1,1,1,0,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,1,1,1,0,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,1,1,1,0,0,0}};
+
+byte C[8][8]={{0,0,0,0,0,0,0,0},
+              {0,0,0,1,1,1,0,0},
+              {0,0,1,0,0,0,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,0,1,0,0,0,0,0},
+              {0,0,0,1,1,1,0,0}};
+
+
+byte D[8][8]={{0,0,0,0,0,0,0,0},
+              {0,1,1,1,1,0,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,0,0,0,0,1,0},
+              {0,1,0,0,0,0,1,0},
+              {0,1,0,0,0,0,1,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,1,1,1,0,0,0}};
+
+
+byte E[8][8]={{0,0,0,0,0,0,0,0},
+              {0,1,1,1,1,1,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,1,1,1,0,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,1,1,1,1,0,0}};
+
+
+byte F[8][8]={{0,0,0,0,0,0,0,0},
+              {0,1,1,1,1,1,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,1,1,1,0,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,0,0,0,0,0,0}};
+
+byte G[8][8]={{0,0,0,0,0,0,0,0},
+              {0,0,1,1,1,1,0,0},
+              {0,1,0,0,0,0,1,0},
+              {0,1,0,0,0,0,0,0},
+              {0,1,0,1,1,1,1,0},
+              {0,1,0,0,0,0,1,0},
+              {0,1,0,0,0,1,0,0},
+              {0,0,1,1,1,0,0,0}};
+
+byte H[8][8]={{0,0,0,0,0,0,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,1,1,1,1,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,0,0,0,1,0,0}};
+              
+byte I[8][8]={{0,0,0,0,0,0,0,0},
+              {0,0,1,1,1,0,0,0},
+              {0,0,0,1,0,0,0,0},
+              {0,0,0,1,0,0,0,0},
+              {0,0,0,1,0,0,0,0},
+              {0,0,0,1,0,0,0,0},
+              {0,0,0,1,0,0,0,0},
+              {0,0,1,1,1,0,0,0}};
+              
+byte J[8][8]={{0,0,0,0,0,0,0,0},
+              {0,0,1,1,1,1,0,0},
+              {0,0,0,0,0,1,0,0},
+              {0,0,0,0,0,1,0,0},
+              {0,0,0,0,0,1,0,0},
+              {0,0,0,0,0,1,0,0},
+              {0,0,1,0,0,1,0,0},
+              {0,0,0,1,1,0,0,0}};
+              
+byte K[8][8]={{0,0,0,0,0,0,0,0},
+              {0,1,0,0,0,1,0,0},
+              {0,1,0,0,1,0,0,0},
+              {0,1,0,1,0,0,0,0},
+              {0,1,1,0,0,0,0,0},
+              {0,1,0,1,0,0,0,0},
+              {0,1,0,0,1,0,0,0},
+              {0,1,0,0,0,1,0,0}};
+              
 
 //Definiëren van de uitganspinnen
 const int shiftClockPin = 3;  //SH
@@ -49,9 +134,9 @@ int K8 = 13;
 int K7 = 16;
 int KS = 0;
 word bericht1  = 0b1101011000010110;
-int K = 0;
+int Kol = 0;
 unsigned long previousMillis = 0;  
-const long interval = 500; 
+long interval = 500; 
 boolean shiftLeftRight = false;
 
 void setup() {
@@ -67,12 +152,21 @@ void loop(){
  unsigned long currentMillis = millis();
  if (currentMillis - previousMillis >= interval) {
    Serial.println(i);
-    i = (i+1)%2;
+    i = (i+1)%11;
     previousMillis = currentMillis;
 
     switch(i){
       case(0): Acpy(A); break;
-      case(1): Acpy(J); break;
+      case(1): Acpy(B); break;
+      case(2): Acpy(C); break;
+      case(3): Acpy(D); break;
+      case(4): Acpy(E); break;
+      case(5): Acpy(F); break;
+      case(6): Acpy(G); break;
+      case(7): Acpy(H); break;
+      case(8): Acpy(I); break;
+      case(9): Acpy(J); break;
+      case(10): Acpy(K); break;
       }
  }
  KS= 0;
@@ -99,7 +193,7 @@ void displayData(word message)
 
 void Translate(){
 
-    switch(K){
+    switch(Kol){
       case (0): KS=K1;break;
       case (1): KS=K2;break;
       case (2): KS=K3;break;
@@ -109,41 +203,41 @@ void Translate(){
       case (6): KS=K7;break;
       case (7): KS=K8;break;
       }
-  if (IMAGE[0][K] == 1){
+  if (IMAGE[0][Kol] == 1){
     bericht1 = setBits(KS, 1, bericht1);
     bericht1 = setBits(1, 0, bericht1);
     }
-  if (IMAGE[1][K] == 1){
+  if (IMAGE[1][Kol] == 1){
     bericht1 = setBits(KS, 1, bericht1);
     bericht1 = setBits(2, 0, bericht1);
     }
-  if (IMAGE[2][K] == 1){
+  if (IMAGE[2][Kol] == 1){
     bericht1 = setBits(KS, 1, bericht1);
     bericht1 = setBits(6, 0, bericht1);
     }
-  if (IMAGE[3][K] == 1){
+  if (IMAGE[3][Kol] == 1){
     bericht1 = setBits(KS, 1, bericht1);
     bericht1 = setBits(12, 0, bericht1);
     }
-  if (IMAGE[4][K] == 1){
+  if (IMAGE[4][Kol] == 1){
     bericht1 = setBits(KS, 1, bericht1);
     bericht1 = setBits(7, 0, bericht1);
     }
-  if (IMAGE[5][K] == 1){
+  if (IMAGE[5][Kol] == 1){
     bericht1 = setBits(KS, 1, bericht1);
     bericht1 = setBits(14, 0, bericht1);
     }
-  if (IMAGE[6][K] == 1){
+  if (IMAGE[6][Kol] == 1){
     bericht1 = setBits(KS, 1, bericht1);
     bericht1 = setBits(15, 0, bericht1);
     }
-  if (IMAGE[7][K] == 1){
+  if (IMAGE[7][Kol] == 1){
     bericht1 = setBits(KS, 1, bericht1);
     bericht1 = setBits(4, 0, bericht1);
     }
-    K++;
-  if (K > 8) {
-    K=0;}
+    Kol++;
+  if (Kol > 8) {
+    Kol=0;}
     
 }
 
